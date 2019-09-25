fc614e258aab45e4207191d774227b5b6cee6be5
dingzhencong <dingzhencong@gmail.com>
2019-09-25 15:05:14 +0800

    两整数之和

---
+++ src/leetcode/editor/cn/SumOfTwoIntegers_371.java
@@ -0,0 +1,32 @@
+package leetcode.editor.cn;
+//不使用运算符 + 和 - ，计算两整数 a 、b 之和。 
+//
+// 示例 1: 
+//
+// 输入: a = 1, b = 2
+//输出: 3
+// 
+//
+// 示例 2: 
+//
+// 输入: a = -2, b = 3
+//输出: 1 
+// Related Topics 位运算
+
+//两整数之和
+class SumOfTwoIntegers_371{
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int getSum(int a, int b) {
+        while(b != 0){
+            int temp = a ^ b;
+            b = (a & b) << 1;
+            a = temp;
+        }
+        return a;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
