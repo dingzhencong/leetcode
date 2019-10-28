3317907e2673e8d7c6231271bdb988eff63ee6c5
dingzhencong <dingzhencong@gmail.com>
2019-10-28 18:41:10 +0800

    字符串中的单词数

---
+++ src/leetcode/editor/cn/NumberOfSegmentsInAString_434.java
@@ -0,0 +1,29 @@
+package leetcode.editor.cn;
+//统计字符串中的单词个数，这里的单词指的是连续的不是空格的字符。 
+//
+// 请注意，你可以假定字符串里不包括任何不可打印的字符。 
+//
+// 示例: 
+//
+// 输入: "Hello, my name is John"
+//输出: 5
+//解释: 这里的单词是指连续的不是空格的字符，所以 "Hello," 算作 1 个单词。
+// 
+// Related Topics 字符串
+
+//字符串中的单词数
+class NumberOfSegmentsInAString_434{
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int countSegments(String s) {
+        String trimmed = s.trim();
+        if (trimmed.equals("")) {
+            return 0;
+        }
+        return trimmed.split("\\s+").length;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
