f691ff4a93682b43e473ffda5cf7074d992f6066
dingzhencong <dingzhencong@gmail.com>
2020-01-12 16:30:15 +0800

    判断是否回文链表

---
+++ src/leetcode/editor/cn/PalindromeLinkedList_234.java
@@ -0,0 +1,67 @@
+package leetcode.editor.cn;
+//请判断一个链表是否为回文链表。 
+//
+// 示例 1: 
+//
+// 输入: 1->2
+//输出: false 
+//
+// 示例 2: 
+//
+// 输入: 1->2->2->1
+//输出: true
+// 
+//
+// 进阶： 
+//你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？ 
+// Related Topics 链表 双指针 
+// 👍 807 👎 0
+
+//回文链表
+class PalindromeLinkedList_234{
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode right = reverseList(slow);
+        ListNode left = head;
+        while (right != null) {
+            if (right.val != left.val) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
