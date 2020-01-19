dae499dc5fd5021dc17aefde6e590298aeadd33f
dingzhencong <dingzhencong@gmail.com>
2020-01-19 13:10:39 +0800

    二叉树展开为链表

---
+++ src/leetcode/editor/cn/FlattenBinaryTreeToLinkedList_114.java
@@ -0,0 +1,71 @@
+package leetcode.editor.cn;
+//给定一个二叉树，原地将它展开为一个单链表。 
+//
+// 
+//
+// 例如，给定二叉树 
+//
+//     1
+//   / \
+//  2   5
+// / \   \
+//3   4   6 
+//
+// 将其展开为： 
+//
+// 1
+// \
+//  2
+//   \
+//    3
+//     \
+//      4
+//       \
+//        5
+//         \
+//          6 
+// Related Topics 树 深度优先搜索 
+// 👍 672 👎 0
+
+//二叉树展开为链表
+class FlattenBinaryTreeToLinkedList_114{
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        flatten(root.left);
+        flatten(root.right);
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        root.left = null;
+        root.right = left;
+        TreeNode p = root;
+        while (p.right != null) {
+            p = p.right;
+        }
+        p.right = right;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
