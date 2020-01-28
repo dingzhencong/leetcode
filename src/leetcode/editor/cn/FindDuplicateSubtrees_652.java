4b49e917c8cf893521c3e7fdd0ea8d73457a0163
dingzhencong <dingzhencong@gmail.com>
2020-01-28 15:02:50 +0800

    二叉搜索树

---
+++ src/leetcode/editor/cn/FindDuplicateSubtrees_652.java
@@ -0,0 +1,61 @@
+package leetcode.editor.cn;
+//给定一棵二叉树，返回所有重复的子树。对于同一类的重复子树，你只需要返回其中任意一棵的根结点即可。 
+//
+// 两棵树重复是指它们具有相同的结构以及相同的结点值。 
+//
+// 示例 1： 
+//
+//         1
+//       / \
+//      2   3
+//     /   / \
+//    4   2   4
+//       /
+//      4
+// 
+//
+// 下面是两个重复的子树： 
+//
+//       2
+//     /
+//    4
+// 
+//
+// 和 
+//
+//     4
+// 
+//
+// 因此，你需要以列表的形式返回上述重复子树的根结点。 
+// Related Topics 树 
+// 👍 204 👎 0
+
+import java.util.List;
+
+//寻找重复的子树
+class FindDuplicateSubtrees_652{
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
