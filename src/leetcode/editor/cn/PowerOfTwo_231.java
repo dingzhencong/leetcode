117039191ea46bfabc4c1d149a2e5cfdff78f9fa
dingzhencong <dingzhencong@gmail.com>
2020-02-15 21:15:08 +0800

    优化逻辑

--- src/leetcode/editor/cn/PowerOfTwo_231.java
+++ src/leetcode/editor/cn/PowerOfTwo_231.java
@@ -24,13 +24,14 @@ class PowerOfTwo_231{
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if (n < 0) { return false; }    // 负数
-        int count = 0;      // 计数器
-        while (n != 0) {
-            if ((n & 0x1) == 1) { count++; }    // 每次取一位判断
-            n >>>= 1;       // 注意使用逻辑右移，不过对于正数，算术右移和逻辑右移效果是一样的
-        }
-        return count == 1 ? true : false;
+        if (n <= 0) { return false; }    // 负数
+//        int count = 0;      // 计数器
+//        while (n != 0) {
+//            if ((n & 0x1) == 1) { count++; }    // 每次取一位判断
+//            n >>>= 1;       // 注意使用逻辑右移，不过对于正数，算术右移和逻辑右移效果是一样的
+//        }
+//        return count == 1 ? true : false;
+        return (n & (n - 1)) == 0;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
