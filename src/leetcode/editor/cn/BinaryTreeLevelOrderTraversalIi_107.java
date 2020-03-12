cd72ce574e453110e9221e856557b2e301af46d0
dingzhencong <dingzhencong@gmail.com>
2020-03-12 15:41:21 +0800

    BT路径总和

---
+++ src/leetcode/editor/cn/BinaryTreeLevelOrderTraversalIi_107.java
@@ -0,0 +1,47 @@
+package leetcode.editor.cn;
+//给定一个二叉树，返回其节点值自底向上的层序遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历） 
+//
+// 例如： 
+//给定二叉树 [3,9,20,null,null,15,7], 
+//
+// 
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回其自底向上的层序遍历为： 
+//
+// 
+//[
+//  [15,7],
+//  [9,20],
+//  [3]
+//]
+// 
+// Related Topics 树 广度优先搜索 
+// 👍 382 👎 0
+
+//二叉树的层序遍历 II
+class BinaryTreeLevelOrderTraversalIi_107{
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
