cd7d910dd17768060effcad5c1233ef8d9c4a59f
dingzhencong <dingzhencong@gmail.com>
2020-03-31 19:53:03 +0800

    左叶子之和

---
+++ src/leetcode/editor/cn/SumOfLeftLeaves_404.java
@@ -0,0 +1,54 @@
+package leetcode.editor.cn;
+//计算给定二叉树的所有左叶子之和。 
+//
+// 示例： 
+//
+// 
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+//在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24 
+//
+// 
+// Related Topics 树 
+// 👍 270 👎 0
+
+//左叶子之和
+class SumOfLeftLeaves_404{
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        return root != null ? dfs(root) : 0;
+    }
+
+    public int dfs(TreeNode node) {
+        int ans = 0;
+        if (node.left != null) {
+            ans += isLeafNode(node.left) ? node.left.val : dfs(node.left);
+        }
+        if (node.right != null && !isLeafNode(node.right)) {
+            ans += dfs(node.right);
+        }
+        return ans;
+    }
+
+    public boolean isLeafNode(TreeNode node) {
+        return node.left == null && node.right == null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
