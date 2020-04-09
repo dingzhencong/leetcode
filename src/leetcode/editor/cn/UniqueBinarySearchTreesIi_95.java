7532721f2254070e3f53cf3c3aa1b044f2acc6d2
dingzhencong <dingzhencong@gmail.com>
2020-04-09 19:58:01 +0800

    不同的二叉搜索树

---
+++ src/leetcode/editor/cn/UniqueBinarySearchTreesIi_95.java
@@ -0,0 +1,97 @@
+package leetcode.editor.cn;
+//给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。 
+//
+// 
+//
+// 示例： 
+//
+// 输入：3
+//输出：
+//[
+//  [1,null,3,2],
+//  [3,2,null,1],
+//  [3,1,null,null,2],
+//  [2,1,3],
+//  [1,null,2,null,3]
+//]
+//解释：
+//以上的输出对应以下 5 种不同结构的二叉搜索树：
+//
+//   1         3     3      2      1
+//    \       /     /      / \      \
+//     3     2     1      1   3      2
+//    /     /       \                 \
+//   2     1         2                 3
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= n <= 8 
+// 
+// Related Topics 树 动态规划 
+// 👍 739 👎 0
+
+import java.util.LinkedList;
+import java.util.List;
+
+//不同的二叉搜索树 II
+class UniqueBinarySearchTreesIi_95{
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<TreeNode>();
+        }
+        return generateTrees(1, n);
+    }
+
+    public List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> allTrees = new LinkedList<TreeNode>();
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+
+        // 枚举可行根节点
+        for (int i = start; i <= end; i++) {
+            // 获得所有可行的左子树集合
+            List<TreeNode> leftTrees = generateTrees(start, i - 1);
+
+            // 获得所有可行的右子树集合
+            List<TreeNode> rightTrees = generateTrees(i + 1, end);
+
+            // 从左子树集合中选出一棵左子树，从右子树集合中选出一棵右子树，拼接到根节点上
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode currTree = new TreeNode(i);
+                    currTree.left = left;
+                    currTree.right = right;
+                    allTrees.add(currTree);
+                }
+            }
+        }
+        return allTrees;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
