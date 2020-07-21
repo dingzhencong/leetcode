a12f4fc128170b9e17355f0e967faa95e9f37fa9
dingzhencong <dingzhencong@gmail.com>
2020-07-21 20:49:11 +0800

    杨辉三角

---
+++ src/leetcode/editor/cn/PascalsTriangle_118.java
@@ -0,0 +1,48 @@
+package leetcode.editor.cn;
+//给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。 
+//
+// 
+//
+// 在杨辉三角中，每个数是它左上方和右上方的数的和。 
+//
+// 示例: 
+//
+// 输入: 5
+//输出:
+//[
+//     [1],
+//    [1,1],
+//   [1,2,1],
+//  [1,3,3,1],
+// [1,4,6,4,1]
+//] 
+// Related Topics 数组 
+// 👍 435 👎 0
+
+import java.util.ArrayList;
+import java.util.List;
+
+//杨辉三角
+class PascalsTriangle_118{
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numRows; ++i) {
+            List<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));
+                }
+            }
+            ret.add(row);
+        }
+        return ret;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
