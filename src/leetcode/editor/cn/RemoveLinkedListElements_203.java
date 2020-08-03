45b89d38269a5174c270d4c1e7000d77d9c5bf5d
dingzhencong <dingzhencong@gmail.com>
2020-08-03 20:07:12 +0800

    移除链表元素

---
+++ src/leetcode/editor/cn/RemoveLinkedListElements_203.java
@@ -0,0 +1,40 @@
+package leetcode.editor.cn;
+//删除链表中等于给定值 val 的所有节点。 
+//
+// 示例: 
+//
+// 输入: 1->2->6->3->4->5->6, val = 6
+//输出: 1->2->3->4->5
+// 
+// Related Topics 链表 
+// 👍 508 👎 0
+
+//移除链表元素
+class RemoveLinkedListElements_203{
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode sentinel = new ListNode(0);
+        sentinel.next = head;
+
+        ListNode prev = sentinel, curr = head;
+        while (curr != null) {
+            if (curr.val == val) prev.next = curr.next;
+            else prev = curr;
+            curr = curr.next;
+        }
+        return sentinel.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
---
+++ src/leetcode/editor/cn/Sqrtx_69.java
@@ -0,0 +1,35 @@
+package leetcode.editor.cn;
+//实现 int sqrt(int x) 函数。 
+//
+// 计算并返回 x 的平方根，其中 x 是非负整数。 
+//
+// 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。 
+//
+// 示例 1: 
+//
+// 输入: 4
+//输出: 2
+// 
+//
+// 示例 2: 
+//
+// 输入: 8
+//输出: 2
+//说明: 8 的平方根是 2.82842..., 
+//     由于返回类型是整数，小数部分将被舍去。
+// 
+// Related Topics 数学 二分查找 
+// 👍 568 👎 0
+
+//x 的平方根
+class Sqrtx_69{
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int mySqrt(int x) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
