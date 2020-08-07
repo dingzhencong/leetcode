4bbb8d73fd71b8eaf58d918bb575e1293fc8bbfd
dingzhencong <dingzhencong@gmail.com>
2020-08-07 21:14:37 +0800

    存在重复元素!!

---
+++ src/leetcode/editor/cn/ContainsDuplicateIi_219.java
@@ -0,0 +1,47 @@
+package leetcode.editor.cn;
+//给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的 绝对值
+// 至多为 k。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: nums = [1,2,3,1], k = 3
+//输出: true 
+//
+// 示例 2: 
+//
+// 输入: nums = [1,0,1,1], k = 1
+//输出: true 
+//
+// 示例 3: 
+//
+// 输入: nums = [1,2,3,1,2,3], k = 2
+//输出: false 
+// Related Topics 数组 哈希表 
+// 👍 229 👎 0
+
+import java.util.HashSet;
+
+//存在重复元素 II
+class ContainsDuplicateIi_219{
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+            if(set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
