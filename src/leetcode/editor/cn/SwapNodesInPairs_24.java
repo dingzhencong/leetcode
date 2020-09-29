f62d18f1cc5033485c7c36332913e32a6763b794
dingzhencong <dingzhencong@gmail.com>
2020-09-29 16:09:10 +0800

    两两交换链表中的节点

---
+++ src/leetcode/editor/cn/SwapNodesInPairs_24.java
@@ -0,0 +1,67 @@
+package leetcode.editor.cn;
+//给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。 
+//
+// 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4]
+//输出：[2,1,4,3]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1]
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 100] 内 
+// 0 <= Node.val <= 100 
+// 
+// Related Topics 递归 链表 
+// 👍 776 👎 0
+
+//两两交换链表中的节点
+class SwapNodesInPairs_24{
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = head.next;
+        head.next = swapPairs(newHead.next);
+        newHead.next = head;
+        return newHead;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
