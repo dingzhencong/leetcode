d526315060560c90afe8b4705e9e209ce3337859
dingzhencong <dingzhencong@gmail.com>
2020-10-10 13:25:47 +0800

    BTP

---
+++ src/leetcode/editor/cn/BinaryTreePaths_257.java
@@ -0,0 +1,44 @@
+package leetcode.editor.cn;
+//给定一个二叉树，返回所有从根节点到叶子节点的路径。 
+//
+// 说明: 叶子节点是指没有子节点的节点。 
+//
+// 示例: 
+//
+// 输入:
+//
+//   1
+// /   \
+//2     3
+// \
+//  5
+//
+//输出: ["1->2->5", "1->3"]
+//
+//解释: 所有根节点到叶子节点的路径为: 1->2->5, 1->3 
+// Related Topics 树 深度优先搜索 
+// 👍 421 👎 0
+
+import java.util.List;
+
+//二叉树的所有路径
+class BinaryTreePaths_257{
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
