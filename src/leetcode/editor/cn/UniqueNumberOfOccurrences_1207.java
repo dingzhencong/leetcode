a547cc4b2a0d7708f7b406210a9190a4642ce756
dingzhencong <dingzhencong@gmail.com>
2020-11-12 22:21:18 +0800

    array

---
+++ src/leetcode/editor/cn/UniqueNumberOfOccurrences_1207.java
@@ -0,0 +1,61 @@
+package leetcode.editor.cn;
+//给你一个整数数组 arr，请你帮忙统计数组中每个数的出现次数。 
+//
+// 如果每个数的出现次数都是独一无二的，就返回 true；否则返回 false。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：arr = [1,2,2,1,1,3]
+//输出：true
+//解释：在该数组中，1 出现了 3 次，2 出现了 2 次，3 只出现了 1 次。没有两个数的出现次数相同。 
+//
+// 示例 2： 
+//
+// 输入：arr = [1,2]
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 输入：arr = [-3,0,1,-3,1,1,1,-3,10,0]
+//输出：true
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= arr.length <= 1000 
+// -1000 <= arr[i] <= 1000 
+// 
+// Related Topics 哈希表 
+// 👍 111 👎 0
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+//独一无二的出现次数
+class UniqueNumberOfOccurrences_1207{
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> occur = new HashMap<Integer, Integer>();
+        for (int x : arr) {
+            occur.put(x, occur.getOrDefault(x, 0) + 1);
+        }
+        Set<Integer> times = new HashSet<Integer>();
+        for (Map.Entry<Integer, Integer> x : occur.entrySet()) {
+            times.add(x.getValue());
+        }
+        return times.size() == occur.size();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
