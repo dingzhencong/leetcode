316d98564afc7a2acb16182bc9457da9fd094f51
dingzhencong <dingzhencong@gmail.com>
2020-12-15 17:20:08 +0800

    递增顺序查找树

---
+++ src/leetcode/editor/cn/IncreasingOrderSearchTree_897.java
@@ -0,0 +1,92 @@
+package leetcode.editor.cn;
+//给你一个树，请你 按中序遍历 重新排列树，使树中最左边的结点现在是树的根，并且每个结点没有左子结点，只有一个右子结点。 
+//
+// 
+//
+// 示例 ： 
+//
+// 输入：[5,3,6,2,4,null,8,1,null,null,null,7,9]
+//
+//       5
+//      / \
+//    3    6
+//   / \    \
+//  2   4    8
+// /        / \ 
+//1        7   9
+//
+//输出：[1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+//
+// 1
+//  \
+//   2
+//    \
+//     3
+//      \
+//       4
+//        \
+//         5
+//          \
+//           6
+//            \
+//             7
+//              \
+//               8
+//                \
+//                 9  
+//
+// 
+//
+// 提示： 
+//
+// 
+// 给定树中的结点数介于 1 和 100 之间。 
+// 每个结点都有一个从 0 到 1000 范围内的唯一整数值。 
+// 
+// Related Topics 树 深度优先搜索 递归 
+// 👍 130 👎 0
+
+import java.util.ArrayList;
+import java.util.List;
+
+//递增顺序查找树
+class IncreasingOrderSearchTree_897{
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode increasingBST(TreeNode root) {
+        List<Integer> vals = new ArrayList();
+        inorder(root, vals);
+        TreeNode ans = new TreeNode(0), cur = ans;
+        for (int v: vals) {
+            cur.right = new TreeNode(v);
+            cur = cur.right;
+        }
+        return ans.right;
+    }
+
+    public void inorder(TreeNode node, List<Integer> vals) {
+        if (node == null) return;
+        inorder(node.left, vals);
+        vals.add(node.val);
+        inorder(node.right, vals);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
