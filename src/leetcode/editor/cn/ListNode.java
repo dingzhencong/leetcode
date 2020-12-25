e99583571ecad14b283ec0308da6394886262a67
dingzhencong <dingzhencong@gmail.com>
2020-12-25 14:26:46 +0800

    code

--- src/leetcode/editor/cn/ListNode.java
+++ src/leetcode/editor/cn/ListNode.java
@@ -4,4 +4,9 @@ public class ListNode {
     int val;
     ListNode next;
     ListNode(int x) { val = x; }
+
+    ListNode(int x, ListNode next) {
+        this.next = next;
+        val = x;
+    }
 }
--- src/leetcode/editor/cn/LongestPalindromicSubstring.java
+++ src/leetcode/editor/cn/LongestPalindromicSubstring.java
@@ -16,17 +16,41 @@
 // Related Topics 字符串 动态规划
 
 package leetcode.editor.cn;
+
+import java.util.HashMap;
+
 public class LongestPalindromicSubstring{
     public static void main(String[] args){
         Solution solution = new LongestPalindromicSubstring().new Solution();
-        
+        HashMap map = new HashMap(3);
+        map.put(2, 2);
+        map.put(3, 3);
+        map.put(4, 4);
     }
     //leetcode submit region begin(Prohibit modification and deletion)
     @SuppressWarnings("AlibabaCommentsMustBeJavadocFormat")
     class Solution {
-    public String longestPalindrome(String s) {
-        return null;
-    }
+        public String longestPalindrome(String s) {
+            int n = s.length();
+            boolean[][] dp = new boolean[n][n];
+            String ans = "";
+            for (int l = 0; l < n; ++l) {
+                for (int i = 0; i + l < n; ++i) {
+                    int j = i + l;
+                    if (l == 0) {
+                        dp[i][j] = true;
+                    } else if (l == 1) {
+                        dp[i][j] = (s.charAt(i) == s.charAt(j));
+                    } else {
+                        dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]);
+                    }
+                    if (dp[i][j] && l + 1 > ans.length()) {
+                        ans = s.substring(i, i + l + 1);
+                    }
+                }
+            }
+            return ans;
+        }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
