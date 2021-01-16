c13b5da644f095eea6c5628f56395a8c1f6b2a3a
dingzhencong <dingzhencong@gmail.com>
2021-01-16 14:11:02 +0800

    寻找旋转排序数组中的最小值

---
+++ src/leetcode/editor/cn/FindMinimumInRotatedSortedArray_153.java
@@ -0,0 +1,83 @@
+package leetcode.editor.cn;
+//假设按照升序排序的数组在预先未知的某个点上进行了旋转。例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] 。 
+//
+// 请找出其中最小的元素。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [3,4,5,1,2]
+//输出：1
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [4,5,6,7,0,1,2]
+//输出：0
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [1]
+//输出：1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 5000 
+// -5000 <= nums[i] <= 5000 
+// nums 中的所有整数都是 唯一 的 
+// nums 原来是一个升序排序的数组，但在预先未知的某个点上进行了旋转 
+// 
+// Related Topics 数组 二分查找 
+// 👍 325 👎 0
+
+import java.io.FileReader;
+
+//寻找旋转排序数组中的最小值
+class FindMinimumInRotatedSortedArray_153{
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    public int findMin(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+        if (nums[right] > nums[0]) {
+            return nums[0];
+        }
+        /**
+         * [4,5,6,7,0,1,2]
+         */
+        while (right > left) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            }
+            if (nums[mid] < nums[mid - 1]) {
+                return nums[mid];
+            }
+            if (nums[mid] > nums[0]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
