c7334dd7eb4d68769ea2ced6116b6c2c72be7376
dingzhencong <dingzhencong@gmail.com>
2021-01-28 21:57:05 +0800

    fix

---
+++ src/leetcode/editor/cn/ValidPalindromeIi_680.java
@@ -0,0 +1,38 @@
+package leetcode.editor.cn;
+//给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。 
+//
+// 示例 1: 
+//
+// 
+//输入: "aba"
+//输出: True
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: "abca"
+//输出: True
+//解释: 你可以删除c字符。
+// 
+//
+// 注意: 
+//
+// 
+// 字符串只包含从 a-z 的小写字母。字符串的最大长度是50000。 
+// 
+// Related Topics 字符串 
+// 👍 313 👎 0
+
+//验证回文字符串 Ⅱ
+class ValidPalindromeIi_680{
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean validPalindrome(String s) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
