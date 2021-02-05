78599d1bc5ab03f514465b644cb01f5ef69e9515
dingzhencong <dingzhencong@gmail.com>
2021-02-05 11:29:52 +0800

    add file

---
+++ src/leetcode/editor/cn/Candy_135.java
@@ -0,0 +1,68 @@
+package leetcode.editor.cn;
+//老师想给孩子们分发糖果，有 N 个孩子站成了一条直线，老师会根据每个孩子的表现，预先给他们评分。 
+//
+// 你需要按照以下要求，帮助老师给这些孩子分发糖果： 
+//
+// 
+// 每个孩子至少分配到 1 个糖果。 
+// 评分更高的孩子必须比他两侧的邻位孩子获得更多的糖果。 
+// 
+//
+// 那么这样下来，老师至少需要准备多少颗糖果呢？ 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：[1,0,2]
+//输出：5
+//解释：你可以分别给这三个孩子分发 2、1、2 颗糖果。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：[1,2,2]
+//输出：4
+//解释：你可以分别给这三个孩子分发 1、2、1 颗糖果。
+//     第三个孩子只得到 1 颗糖果，这已满足上述两个条件。 
+// Related Topics 贪心算法 
+// 👍 468 👎 0
+
+//分发糖果
+class Candy_135{
+    public static void main(String[] args) {
+        int[] left = new int[10];
+        for (int i : left) {
+            System.out.println(i);
+        }
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] left = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            } else {
+                left[i] = 1;
+            }
+        }
+        int right = 0, ret = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i < n - 1 && ratings[i] > ratings[i + 1]) {
+                right++;
+            } else {
+                right = 1;
+            }
+            ret += Math.max(left[i], right);
+        }
+        return ret;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
